8bb0bdfd447a40effcd2ef1760999f205b803c4d
Minyoung Kim <miny5871@naver.com>
2016-01-15 13:49:07 +0900

    #7 POST 작동연습

---
+++ postLogout.php
@@ -0,0 +1,6 @@
+<?php
+setCookie('islogin','0');
+setCookie('name','');
+$url = './postForm.php';
+header("Location: ".$url);
+?>
--- postResult.php
+++ postResult.php
@@ -2,13 +2,39 @@
 header("Content-Type: text/html; charset=UTF-8");
 
 $name = $_POST['name'];
-$year = $_POST['year'];
-$mon = $_POST['mon'];
-$day = $_POST['day'];
-$age = 2016-$year+1;
+$pw = $_POST['pw'];
+
 ?>
 
-<h1>WoW!</h1>
-I know you, <?=$name?>!!<br>
-Your birthday is <?=$year?>-<?=$mon?>-<?=$day?><br>
-your age : <?=$age?> years old.
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+
+
+<?php
+if( $name == "miny" && $pw == "kiyomi"){
+
+setCookie('islogin','1');
+setCookie('name',$name);
+?>
+<h1>로그인 성공!</h1>
+
+당신의 이름은 <?=$name?>이고, 암호는 <?=$pw?>입니당.
+로그인 페이지로 <a href = "./postForm.php">돌아가기 </a>
+<?php
+}else{
+setCookie('islogin','0');
+setCookie('name', '');
+?>
+잘못된 아이디와 암호입니다. <br>
+다시 로그인 해보세요! <br>
+<?php
+}
+?>
+
+
